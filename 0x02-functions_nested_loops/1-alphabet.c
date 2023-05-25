fc2e605a3af8cf37c9dfb1c26843cabac655bab9
d3vpablo <ehimarepaul@yahoo.com>
2023-05-24 17:03:25 -0700

    added task 1 and 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ */
+
+void print_alphabet(void)
+{
+	int i = 'a';
+
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
